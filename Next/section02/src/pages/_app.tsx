61ecbd35cf801ba45d7f1ec4ed0e761189c2f60e
yongchane <202021113@sangmyung.kr>
2024-12-23 00:23:02 +0900

    api routes

--- Next/section02/src/pages/_app.tsx
+++ Next/section02/src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClickHandler = () => {
+    router.push("/test");
+  };
+
+  useEffect(
+    () => {
+      router.prefetch("/test");
+    } // 어떤 페이지를 prefetch할건지 정해주면 됌
+  );
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        <Link href={"/search"}>search</Link>
+      </header>
+      <button onClick={onClickHandler}> 클릭</button>
+      <Component {...pageProps} />;
+    </>
+  );
 }
