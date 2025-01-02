1544b9175b1e0f0cc2251a0e7f0330ba34c7d173
yongchane <202021113@sangmyung.kr>
2025-01-02 15:01:53 +0900

    서버 컴포넌트, 클라이언트 컴포넌트

--- section03/src/app/(with-searchbar)/page.tsx
+++ section03/src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,14 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-componnet";
 
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
